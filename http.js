22f44138923284e26b0a2e36e883b526a3f4920f
logan <1187023819@qq.com>
2018-08-19 00:07:56 +0800

    first commit

---
+++ http.js
@@ -0,0 +1,13 @@
+var http = require('http');
+
+http
+	.createServer(function(req, res) {
+		//通过response来返回一个响应头
+		//第一个参数是状态码，第二个参数是对象字面量
+		res.writeHead(200， {'Content-Type': 'text/plain'});
+		//写入相应的主体
+		res.write('Hello Nodejs');
+		//结束掉响应
+		res.end();
+	})
+	.listen(2018)
